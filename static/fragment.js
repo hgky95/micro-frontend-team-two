fe940b004aa3cf826d7c26cf8f1b13318801b258
hgky95 <hgky95@gmail.com>
2020-12-23 17:27:51 +0700

    remove unused load and update product items

--- static/fragment.js
+++ static/fragment.js
@@ -48,7 +48,6 @@ class ProductItem extends HTMLElement {
     }
 
     render() {
-        console.log('load');
         const products = this.fetchProduct();
         const productType = this.getAttribute("product-type");
         const product = products[productType];
@@ -73,15 +72,21 @@ class ProductItem extends HTMLElement {
 
     fetchProduct() {
         return {
-            airpods: { name: "Airpods", price: "199", img: "airpods.jpg" },
-            shoe: { name: "Air Jordans", price: "125", img: "air-jordans.jpg" },
-            iphone: { name: "Smartphone", price: "699", img: "smartphone.jpg" },
-            book: { name: "Book", price: "15", img: "book.jpg" },
-            bike: { name: "Bike", price: "800", img: "bike.jpg" },
-            videoGame: { name: "Gaming Console", price: "299", img: "video-game.jpg" },
-            movieTicket: { name: "Movie Ticket", price: "12", img: "movie-ticket.jpg" },
-            amazonEcho: { name: "Amazon Echo", price: "99", img: "amazon-echo.jpg" },
-            drone: { name: "Drone", price: "350", img: "drone.jpg" }
+            "big-mac": { "name": "Big Mac", "price": "20", "img": "big-mac.jpg" },
+            "flip-flops": { "name": "Flip Flops", "price": "30", "img": "flip-flops.jpg" },
+            "coca-cola-pack": { "name": "Coca-Cola Pack", "price": "50", "img": "coca-cola-pack.jpg" },
+            "movie-ticket": { "name": "Movie Ticket", "price": "12000", "img": "movie-ticket.jpg" },
+            "book": { "name": "Book", "price": "150", "img": "book.jpg" },
+            "lobster-dinner": { "name": "Lobster Dinner", "price": "450", "img": "lobster-dinner.jpg" },
+            "video-game": { "name": "Video Game", "price": "600", "img": "video-game.jpg" },
+            "amazon-echo": { "name": "Amazon Echo", "price": "99000", "img": "amazon-echo.jpg" },
+            "year-of-netflix": { "name": "Year of Netflix", "price": "1000", "img": "year-of-netflix.jpg" },
+            "air-jordans": { "name": "Air Jordans", "price": "125000", "img": "air-jordans.jpg" },
+            "airpods": { "name": "Airpods", "price": "199", "img": "airpods.jpg" },
+            "gaming-console": { "name": "Gaming Console", "price": "299", "img": "gaming-console.jpg" },
+            "drone": { "name": "Drone", "price": "350", "img": "drone.jpg" },
+            "smartphone": { "name": "Smartphone", "price": "699", "img": "smartphone.jpg" },
+            "bike": { "name": "Bike", "price": "800", "img": "bike.jpg" }
         };
     }
 }
